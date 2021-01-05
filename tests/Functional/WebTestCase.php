39f62fba268857d31ef3a24713186cd46e663800
tyanrv <tyanrv@gmail.com>
2021-01-05 15:44:48 +0600

    add login() method

--- tests/Functional/WebTestCase.php
+++ tests/Functional/WebTestCase.php
@@ -6,6 +6,9 @@ declare(strict_types=1);
 namespace Test\Functional;
 
 use PHPUnit\Framework\TestCase;
+use Proxy\OAuth\Action\LoginAction;
+use Proxy\OAuth\Action\Type\PasswordType;
+use Proxy\OAuth\Action\Type\UsernameType;
 use Proxy\OAuth\Helpers\DotEnvConfigStorage;
 use Proxy\OAuth\Helpers\GuzzleHttpClient;
 use Proxy\OAuth\Interfaces\ConverterInterface;
@@ -32,5 +35,15 @@ class WebTestCase extends TestCase
 
         $this->configStore = new DotEnvConfigStorage(__DIR__ . '/../../');
         $this->configStore->load();
+    }
+
+    protected function login(): array
+    {
+        $authAction = new LoginAction($this->converter, $this->configStore, $this->httpClient);
+
+        $username = new UsernameType('tyanrv');
+        $password = new PasswordType('hash');
+
+        return $authAction->login($username, $password);
     }
 }
