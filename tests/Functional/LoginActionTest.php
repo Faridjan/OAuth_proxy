1611026a057d294647691d815964859ef5ad217c
tyanrv <tyanrv@gmail.com>
2021-01-05 15:34:05 +0600

    make && finish LoginActionTest.php

--- tests/Functional/LoginActionTest.php
+++ tests/Functional/LoginActionTest.php
@@ -6,6 +6,7 @@ declare(strict_types=1);
 namespace Test\Functional;
 
 
+use Exception;
 use Proxy\OAuth\Action\LoginAction;
 use Proxy\OAuth\Action\Type\PasswordType;
 use Proxy\OAuth\Action\Type\UsernameType;
@@ -20,6 +21,29 @@ class LoginActionTest extends WebTestCase
         $username = new UsernameType('tyanrv');
         $password = new PasswordType('hash');
 
-        print_r($authAction->login($username, $password));
+        $result = $authAction->login($username, $password);
+
+        self::assertTrue(is_array($result));
+
+        self::assertArrayHasKey('token_type', $result);
+        self::assertArrayHasKey('expires_in', $result);
+        self::assertArrayHasKey('access_token', $result);
+        self::assertArrayHasKey('refresh_token', $result);
+
+        self::assertEquals('Bearer', $result['token_type']);
+    }
+
+    public function testInvalid(): void
+    {
+        $authAction = new LoginAction($this->converter, $this->configStore, $this->httpClient);
+
+        $username = new UsernameType('user');
+        $password = new PasswordType('password');
+
+        $this->expectException(Exception::class);
+        $this->expectExceptionMessage('The User Entity not found, check user, domain credentials.');
+        $this->expectExceptionCode(400);
+
+        $authAction->login($username, $password);
     }
 }
